502a1d01afbff844cddeb3583b48ae96d257f924
bdav87 <bdav87@gmail.com>
2018-05-19 16:50:28 -0500

    test: set up a basic api route that uses store auth

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const BigCommerce = require('node-bigcommerce');
+const mysql = require('mysql');
+
+let bc;
+
+
+router.get('/single', (req,res) => {
+    const connection = mysql.createConnection({
+        host: process.env.SQLHOST,
+        user: process.env.SQLUN,
+        password: process.env.SQLPW,
+        database: 'cat_app_db'
+      });
+    
+    connection.connect();
+    
+    connection.query('SELECT * FROM bc_config WHERE id=1', (error, results) => {
+        let accessToken, hash, clientId, secret;
+    
+        if (error) {
+            throw `query fail${error}`;
+        }
+        const db_result = results[0];
+        
+        accessToken = db_result.access_token;
+        hash = db_result.hash;
+        clientId = db_result.client_id;
+        secret = db_result.secret;
+    
+        verifyAndRender(accessToken, hash, clientId, secret);
+    })
+    
+    function verifyAndRender(accessToken, hash, clientId, secret) {
+        connection.end();
+    
+        bc = new BigCommerce({
+            clientId: clientId,
+            secret: secret,
+            hash: hash,
+            accessToken: accessToken,
+            responseType: 'json',
+            apiVersion: 'v3'
+        });
+        res.send(bc);
+    }
+    
+})
+
+module.exports = router;
