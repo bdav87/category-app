12c521d8edbc9ad35f212cc63aa7c5f966eb9156
bdav87 <bdav87@gmail.com>
2018-05-19 19:15:43 -0500

    ui: drag and drop interface added in import area

---
+++ public/javascripts/client.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function(){
+    DragDrop('#dropTarget', function (files, pos, fileList) {
+        console.log('Here are the dropped files', files)
+        console.log('Dropped at coordinates', pos.x, pos.y)
+        console.log('Here is the raw FileList object if you need it:', fileList)
+      
+        // `files` is an Array!
+        files.forEach(function (file) {
+          console.log(file.name)
+          console.log(file.size)
+          console.log(file.type)
+          console.log(file.lastModifiedData)
+          console.log(file.fullPath) // not real full path due to browser security restrictions
+          console.log(file.path) // in Electron, this contains the actual full path
+      
+          // convert the file to a Buffer that we can use!
+          var reader = new FileReader()
+          reader.addEventListener('load', function (e) {
+            // e.target.result is an ArrayBuffer
+            var arr = new Uint8Array(e.target.result)
+            var buffer = new Buffer(arr)
+      
+            // do something with the buffer!
+          })
+          reader.addEventListener('error', function (err) {
+            console.error('FileReader error' + err)
+          })
+          reader.readAsArrayBuffer(file)
+        })
+      })
+})
