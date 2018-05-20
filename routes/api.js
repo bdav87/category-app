7cb35f1569fa61bb4e7478b269362cdffeb01f2f
Brian Davenport <bdav87@gmail.com>
2018-05-20 14:28:51 -0500

    test: logging to ensure config persists

--- routes/api.js
+++ routes/api.js
@@ -35,12 +35,10 @@ router.get('/single', (req,res) => {
 router.get('/export', (req, res) => {
 
     function exportCategories(bc_api) {
+        console.log(bc_api);
         bc_api.get('/catalog/categories?limit=250')
-        .then(data => {
-            //res.render('index', {data: JSON.stringify(data), development: true})
-            streamToCSV(data);
-        })
-        .catch(err => res.render('index', {data: JSON.stringify(err), development: true}))
+        .then(data => streamToCSV(data))
+        .catch(err => res.render('index', {data: `error ${JSON.stringify(err)}`, development: true}))
     }
 
     function streamToCSV(category_page){
