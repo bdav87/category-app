10ea737941b316950cc35fc37291d11606cc9c33
Brian Davenport <bdav87@gmail.com>
2018-05-20 16:59:16 -0500

    fix: root path for send file

--- routes/api.js
+++ routes/api.js
@@ -75,7 +75,7 @@ router.get('/export', (req, res) => {
         csvStream.end();
         
         
-        res.sendFile('test.csv', (err) => {
+        res.sendFile('test.csv', { root: __dirname }, (err) => {
             if (err) {
                 console.log(`csv send err: ${err}`)
             }
