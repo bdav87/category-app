012b0ed87325b9b76e273158121fe92da9b07a6a
bdav87 <bdav87@gmail.com>
2018-05-22 16:43:00 -0500

    test: is multer even doing anything

--- routes/api.js
+++ routes/api.js
@@ -111,6 +111,7 @@ router.get('/export', (req, res) => {
 })
 
 router.post('/import', upload.single('csvFile'), (req, res) => {
+    console.log('multer?', upload);
     console.log('file? ', req.file);
     console.log('file(s)? ', req.files);
     console.log('body? ', req.body);
