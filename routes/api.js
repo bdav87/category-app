b02ece2e21e3d0f2f549c54ae4d79f4408c63848
bdav87 <bdav87@gmail.com>
2018-05-22 15:39:47 -0500

    logs: checking if file uploading in req file instead of body

--- routes/api.js
+++ routes/api.js
@@ -110,8 +110,8 @@ router.get('/export', (req, res) => {
     }
 })
 
-router.post('/import', upload.single('csv'), (req, res) => {
-    console.log('did a file appear: ', csv);
+router.post('/import', upload.single('csvFile'), (req, res) => {
+    console.log('did a file appear: ', csvFile);
     console.log('check req file: ', req.file);
     console.log('check req body: ', req.body);
     res.send('You are importing a file');
