fc1286dcd76b2dc0d51e78c6e5a82ce93d803978
bdav87 <bdav87@gmail.com>
2018-05-22 15:01:49 -0500

    test: appending file data to formdata after declaration instead of in chain

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -34,7 +34,8 @@ document.addEventListener('DOMContentLoaded', function(){
         $('#resultsArea').show().text(file.name);
         console.log('Submitting this file',file);
 
-        const file_to_send = new FormData().append('csvData', file);
+        const file_to_send = new FormData();
+        file_to_send.append('csvData', file);
         console.log(file_to_send);
         $.ajax({
           url: '/api/import',
