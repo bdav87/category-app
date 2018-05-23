44d3785bb0466cee902c04a9d84d90303453df81
bdav87 <bdav87@gmail.com>
2018-05-23 13:41:27 -0500

    fix: make sure to call write category even after failed POST

--- routes/api.js
+++ routes/api.js
@@ -160,11 +160,12 @@ router.post('/import', upload.single('csvFile'), (req, res) => {
             .then(data => {
                 console.log(data);
                 importResults.successful.push(data); 
-                writeCategoryToBC(queue, count, index++)
+                writeCategoryToBC(queue, count, index++);
             })
             .catch(err => {
                 console.log(err);
                 importResults.failed.push(err);
+                writeCategoryToBC(queue, count, index++);
             })
         }
         if (index == count) {
