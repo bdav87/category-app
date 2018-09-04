57ff7a90b7b411358d7c81f2b04fca087bf22845
bdav87 <bdav87@gmail.com>
2018-09-04 14:53:50 -0500

    Remove user from db when permission is revoked

--- routes/remove-user.js
+++ routes/remove-user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const BigCommerce = require('node-bigcommerce');
-
+const mysql = require('mysql');
 
 router.get('/', (req, res) => {
     function initiateVerification(payload) {
@@ -14,12 +14,37 @@ router.get('/', (req, res) => {
             const hash = data.store_hash;
             const user = data.user.email;
             console.log(`Remove user request received for account ${user} on store ${hash}`);
+            removeUser(user, hash);
         } catch(err){
             console.log('Error verifying payload', err)
             return res.status('403').end();
         }
     }
     initiateVerification(req.query['signed_payload']);
+
+    function removeUser(user, hash) {
+        const connection = mysql.createConnection({
+            host: process.env.SQLHOST,
+            user: process.env.SQLUN,
+            password: process.env.SQLPW,
+            database: 'cat_app_db'
+          });
+        
+        connection.connect();
+
+        connection.query(`
+            DELETE FROM cat_app_db.users WHERE configid=(
+            SELECT id from cat_app_db.bc_config WHERE hash=${hash}
+            )
+            AND email=${user}`, (error, result) => {
+                if (error) {
+                    console.log(`Unable to remove user. Error: ${error}`);
+                }
+            });
+        
+        connection.end();
+        
+    }
 
 })
 
